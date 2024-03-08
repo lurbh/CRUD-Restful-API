12a1363010f8b9471a1dbf0359f98399a7b2bb74
lurbh <53887968+lurbh@users.noreply.github.com>
2024-03-08 15:53:58 +0800

    Converted for Mongo

--- index.js
+++ index.js
@@ -3,9 +3,11 @@ const express = require('express');
 // CORS: cross origin resources sharing
 const cors = require('cors');
 const axios = require('axios');
+const mongodb = require('mongodb');
 require('dotenv').config()
 
 const app = express();
+const mongoURI = process.env.MONGODB_CONNECTION_STRING;
 
 // enable CORS (so that other web pages can use our RESTFUl API)
 app.use(cors());
@@ -13,46 +15,195 @@ app.use(cors());
 // enable recieving and sending of JSON
 app.use(express.json());
 
-// const BASE_JSON_BIN_URL = process.env.BASE_JSON_BIN_URL;
-// const BIN_ID = process.env.BIN_ID;
-const appointments = [];
+const MongoClient = mongodb.MongoClient;
+const ObjectId = mongodb.ObjectId;
 
-const APPT_TYPE = [
-    "General Check-up",
-    "Urgent Care Appointment",
-    "Follow-up Appointment",
-    "Chronic Disease Management",
-    "Medication Review Appointment",
-    "Preventive Care Appointment"
-]
+async function connect(uri, dbname) {
+    let client = await MongoClient.connect(uri, {
+        useUnifiedTopology: true
+    })
+    _db = client.db(dbname);
+    return _db;
+}
 
 async function main() 
 {
-    let response = await axios.get(process.env.BASE_JSON_BIN_URL + "/" + process.env.BIN_ID + "/latest");
-    for(let a of response.data.record.Appointment)
-    {
-        const newappt = {
-            "id": a.id,
-            "clinic": a.clinic,
-            "doctor": a.doctor,
-            "appttype": a.appttype,
-            "date": a.date,
-            "time": a.time
+    const db = await connect(mongoURI,'Appointment-Management-System');
+    app.get("/", async function(req,res){
+
+        const appointments = await db.collection("appointments").find({}).toArray();
+        res.status(200);
+        res.json({
+            "message":"Success",
+            "data": appointments
+        })
+    });
+    
+    app.get("/appointments", async function(req,res){
+        const appointments = await db.collection("appointments").find({}).toArray();
+        res.status(200);
+        res.json({
+            "appointments": appointments
+        })
+    });
+    
+    app.post("/appointments", async function(req,res){
+        const clinic = req.body.clinic;
+        const doctor = req.body.doctor;
+        const appttype = req.body.appttype;
+        const date = req.body.date;
+        const time = req.body.time;
+        const datetime = date + "T" + time;
+
+        const typeresult = await db.collection("appointment_type").find({
+            'type' : appttype
+        }).toArray();
+    
+        if (!clinic || !doctor || !appttype || !date || !time) 
+        {
+            res.status(400);
+            res.json({
+                "error":"Incomplete Appointment Details"
+            });
+            return;
         }
-        appointments.push(newappt);
-    }
+        if(typeresult.length == 0)
+        {
+            res.status(400);
+            res.json({
+                "error":"Invalid Appointment Type"
+            });
+            return;
+        }
+        if(!checkDateFormat(date))
+        {
+            res.status(400);
+            res.json({
+                "error":"Invalid Date Given"
+            });
+            return;
+        }
+        if(!checkTimeFormat(time))
+        {
+            res.status(400);
+            res.json({
+                "error":"Invalid Time Given"
+            });
+            return;
+        }
+        try 
+        {
+            const result = db.collection("appointments").insertOne({
+                "clinic": clinic,
+                "doctor": doctor,
+                "appttype": appttype,
+                "datetime": new Date(datetime),
+            });
+            res.json({
+                'message':'Appointment created successfully',
+                'result': result
+            });
+        } catch (error) 
+        {
+            res.status(500); // internal server error
+            res.json({
+                'error': e
+            })
+        }
+    });
+    
+    app.delete("/appointments/:id", function(req,res){
+        try 
+        {
+            const result = db.collection("appointments").deleteOne({
+                "_id" : new ObjectId(req.params.id)
+            });
+            res.json({
+                'message':'Appointment has been deleted',
+                'result': result
+            });
+        } catch (error) 
+        {
+            res.status(500); // internal server error
+            res.json({
+                'error': e
+            })
+        }
+    });
+    
+    app.put("/appointments/:id", async function(req,res){
+        const clinic = req.body.clinic;
+        const doctor = req.body.doctor;
+        const appttype = req.body.appttype;
+        const date = req.body.date;
+        const time = req.body.time;
+        const datetime = date + "T" + time;
+
+        const typeresult = await db.collection("appointment_type").find({
+            'type' : appttype
+        }).toArray();
+    
+        if (!clinic || !doctor || !appttype || !date || !time) 
+        {
+            res.status(400);
+            res.json({
+                "error":"Incomplete Appointment Details"
+            });
+            return;
+        }
+        if(typeresult.length == 0)
+        {
+            res.status(400);
+            res.json({
+                "error":"Invalid Appointment Type"
+            });
+            return;
+        }
+        if(!checkDateFormat(date))
+        {
+            res.status(400);
+            res.json({
+                "error":"Invalid Date Given"
+            });
+            return;
+        }
+        if(!checkTimeFormat(time))
+        {
+            res.status(400);
+            res.json({
+                "error":"Invalid Time Given"
+            });
+            return;
+        }
+        try 
+        {
+            const result = db.collection("appointments").updateOne({
+                "_id" : new ObjectId(req.params.id)
+            },{
+                '$set': {
+                    "clinic": clinic,
+                    "doctor": doctor,
+                    "appttype": appttype,
+                    "datetime": new Date(datetime),
+                }
+            });
+            res.json({
+                'message':'Appointment has been updated successfully',
+                'result': result
+            });
+        } catch (error) 
+        {
+            res.status(500); // internal server error
+            res.json({
+                'error': e
+            })
+        }
+    });
 }
 
 function checkDateFormat(date)
 {
     const sd = date.split('-');
-    // console.log(sd.length);
-    // console.log(sd[0].length);
-    // console.log(sd[0]);
-    // console.log(sd[1].length);
-    // console.log(sd[1]);
-    // console.log(sd[2].length );
-    // console.log(sd[2]);
     if(sd.length == 3)
         if(sd[0].length == 4)
             if(sd[0] >= 2021 && sd[0] <= 2030)
@@ -78,133 +229,6 @@ function checkTimeFormat(time)
 }
   
 main();
-
-app.get("/", function(req,res){
-
-    res.status(200);
-    res.json({
-        "message":"Success",
-        "data": appointments
-    })
-});
-
-app.get("/appointments", function(req,res){
-    res.status(200);
-    res.json({
-        "appointments": appointments
-    })
-});
-
-app.post("/appointments", function(req,res){
-    const clinic = req.body.clinic;
-    const doctor = req.body.doctor;
-    const appttype = req.body.appttype;
-    const date = req.body.date;
-    const time = req.body.time;
-
-    if (!clinic || !doctor || !appttype || !date || !time) 
-    {
-        res.status(400);
-        res.json({
-            "error":"Incomplete Appointment Details"
-        });
-        return;
-    }
-    if(APPT_TYPE.indexOf(appttype) == -1)
-    {
-        res.status(400);
-        res.json({
-            "error":"Invalid Appointment Type"
-        });
-        return;
-    }
-    if(!checkDateFormat(date))
-    {
-        console.log("??")
-        res.status(400);
-        res.json({
-            "error":"Invalid Date Given"
-        });
-        return;
-    }
-    if(!checkTimeFormat(time))
-    {
-        res.status(400);
-        res.json({
-            "error":"Invalid Time Given"
-        });
-        return;
-    }
-    const newappt = {
-        "id": Math.floor(Math.random() * 10000),
-        "clinic": clinic,
-        "doctor": doctor,
-        "appttype": appttype,
-        "date": date,
-        "time": time
-    }
-    appointments.push(newappt);
-    res.status(200);
-    res.json({
-        'message':'Appointment created successfully'
-    });
-});
-
-app.delete("/appointments/:id", function(req,res){
-    const indexToDelete = appointments.findIndex(function(a){
-        return a.id == req.params.id;
-    });
-
-    if(indexToDelete == -1)
-    {
-        res.status(400);
-        res.json({
-            "error":"Appointment ID cannot be found"
-        });
-        return;
-    }
-    appointments.splice(indexToDelete,1);
-    res.status(200);
-    res.json({
-        'message':'Appointment has been deleted'
-    });
-});
-
-app.put("/appointments/:id", function(req,res){
-    const clinic = req.body.clinic;
-    const doctor = req.body.doctor;
-    const appttype = req.body.appttype;
-    const date = req.body.date;
-    const time = req.body.time;
-
-    if (!clinic || !doctor || !appttype || !date || !time) 
-    {
-        res.status(400);
-        res.json({
-            "error":"Incomplete Appointment Details"
-        });
-        return;
-    }
-    const indexToUpdate = appointments.findIndex(function(a){
-        return a.id == req.params.id;
-    });
-
-    const newappt = {
-        "id": parseInt(req.params.id),
-        "clinic": clinic,
-        "doctor": doctor,
-        "appttype": appttype,
-        "date": date,
-        "time": time
-    }
-
-    appointments[indexToUpdate] = newappt;
-    res.status(200);
-    res.json({
-        'message':'Appointment has been updated'
-    });
-});
-
 
 app.listen(9898, function(){
     console.log("server has started at http://localhost:" + 9898);
